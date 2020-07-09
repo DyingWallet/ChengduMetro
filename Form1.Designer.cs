4f54210650c22b757fa32877de12e4f0bed52d77
DyingWallet <709038511@qq.com>
2020-07-09 18:15:21 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace ChengduMetro3._0
+{
+    partial class Mainfrm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mainfrm));
+            this.grpMode = new System.Windows.Forms.GroupBox();
+            this.chkTime = new System.Windows.Forms.CheckBox();
+            this.chkDis = new System.Windows.Forms.CheckBox();
+            this.grpPath = new System.Windows.Forms.GroupBox();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.cmbEnd = new System.Windows.Forms.ComboBox();
+            this.cmbStart = new System.Windows.Forms.ComboBox();
+            this.grpResult = new System.Windows.Forms.GroupBox();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtDis = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.picMap = new System.Windows.Forms.PictureBox();
+            this.grpMode.SuspendLayout();
+            this.grpPath.SuspendLayout();
+            this.grpResult.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpMode
+            // 
+            this.grpMode.Controls.Add(this.chkTime);
+            this.grpMode.Controls.Add(this.chkDis);
+            this.grpMode.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.grpMode.Location = new System.Drawing.Point(12, 12);
+            this.grpMode.Name = "grpMode";
+            this.grpMode.Size = new System.Drawing.Size(120, 80);
+            this.grpMode.TabIndex = 0;
+            this.grpMode.TabStop = false;
+            this.grpMode.Text = "选择模式";
+            // 
+            // chkTime
+            // 
+            this.chkTime.AutoSize = true;
+            this.chkTime.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.chkTime.Location = new System.Drawing.Point(19, 50);
+            this.chkTime.Name = "chkTime";
+            this.chkTime.Size = new System.Drawing.Size(84, 24);
+            this.chkTime.TabIndex = 1;
+            this.chkTime.Text = "时间最短";
+            this.chkTime.UseVisualStyleBackColor = true;
+            this.chkTime.CheckedChanged += new System.EventHandler(this.chkTime_CheckedChanged);
+            // 
+            // chkDis
+            // 
+            this.chkDis.AutoSize = true;
+            this.chkDis.Checked = true;
+            this.chkDis.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDis.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.chkDis.Location = new System.Drawing.Point(19, 20);
+            this.chkDis.Name = "chkDis";
+            this.chkDis.Size = new System.Drawing.Size(84, 24);
+            this.chkDis.TabIndex = 0;
+            this.chkDis.Text = "路程最短";
+            this.chkDis.UseVisualStyleBackColor = true;
+            this.chkDis.CheckedChanged += new System.EventHandler(this.chkDis_CheckedChanged);
+            // 
+            // grpPath
+            // 
+            this.grpPath.Controls.Add(this.btnQuery);
+            this.grpPath.Controls.Add(this.cmbEnd);
+            this.grpPath.Controls.Add(this.cmbStart);
+            this.grpPath.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.grpPath.Location = new System.Drawing.Point(138, 12);
+            this.grpPath.Name = "grpPath";
+            this.grpPath.Size = new System.Drawing.Size(382, 80);
+            this.grpPath.TabIndex = 1;
+            this.grpPath.TabStop = false;
+            this.grpPath.Text = "     出发点                目的地";
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnQuery.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnQuery.Location = new System.Drawing.Point(290, 33);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 2;
+            this.btnQuery.Text = "查询";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // cmbEnd
+            // 
+            this.cmbEnd.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEnd.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cmbEnd.FormattingEnabled = true;
+            this.cmbEnd.Location = new System.Drawing.Point(166, 31);
+            this.cmbEnd.Name = "cmbEnd";
+            this.cmbEnd.Size = new System.Drawing.Size(121, 28);
+            this.cmbEnd.TabIndex = 1;
+            this.cmbEnd.SelectedIndexChanged += new System.EventHandler(this.cmbEnd_SelectedIndexChanged);
+            // 
+            // cmbStart
+            // 
+            this.cmbStart.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbStart.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cmbStart.FormattingEnabled = true;
+            this.cmbStart.Location = new System.Drawing.Point(38, 31);
+            this.cmbStart.Name = "cmbStart";
+            this.cmbStart.Size = new System.Drawing.Size(121, 28);
+            this.cmbStart.TabIndex = 0;
+            this.cmbStart.SelectedIndexChanged += new System.EventHandler(this.cmbStart_SelectedIndexChanged);
+            // 
+            // grpResult
+            // 
+            this.grpResult.Controls.Add(this.txtTime);
+            this.grpResult.Controls.Add(this.txtDis);
+            this.grpResult.Controls.Add(this.txtResult);
+            this.grpResult.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.grpResult.Location = new System.Drawing.Point(526, 12);
+            this.grpResult.Name = "grpResult";
+            this.grpResult.Size = new System.Drawing.Size(490, 80);
+            this.grpResult.TabIndex = 2;
+            this.grpResult.TabStop = false;
+            this.grpResult.Text = "途经站点";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtTime.Location = new System.Drawing.Point(366, 50);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.ReadOnly = true;
+            this.txtTime.Size = new System.Drawing.Size(100, 23);
+            this.txtTime.TabIndex = 2;
+            this.txtTime.Text = "预计耗时";
+            this.txtTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtDis
+            // 
+            this.txtDis.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtDis.Location = new System.Drawing.Point(366, 28);
+            this.txtDis.Name = "txtDis";
+            this.txtDis.ReadOnly = true;
+            this.txtDis.Size = new System.Drawing.Size(100, 23);
+            this.txtDis.TabIndex = 1;
+            this.txtDis.Text = "预计里程";
+            this.txtDis.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtResult
+            // 
+            this.txtResult.CausesValidation = false;
+            this.txtResult.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtResult.Location = new System.Drawing.Point(7, 28);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(337, 46);
+            this.txtResult.TabIndex = 0;
+            // 
+            // picMap
+            // 
+            this.picMap.BackgroundImage = global::ChengduMetro3._0.Properties.Resources.成都地铁线路图1;
+            this.picMap.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picMap.Cursor = System.Windows.Forms.Cursors.No;
+            this.picMap.Location = new System.Drawing.Point(13, 99);
+            this.picMap.Name = "picMap";
+            this.picMap.Size = new System.Drawing.Size(1003, 800);
+            this.picMap.TabIndex = 3;
+            this.picMap.TabStop = false;
+            // 
+            // Mainfrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1034, 911);
+            this.Controls.Add(this.picMap);
+            this.Controls.Add(this.grpResult);
+            this.Controls.Add(this.grpPath);
+            this.Controls.Add(this.grpMode);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Mainfrm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ChengduMetroQuery";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpMode.ResumeLayout(false);
+            this.grpMode.PerformLayout();
+            this.grpPath.ResumeLayout(false);
+            this.grpResult.ResumeLayout(false);
+            this.grpResult.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMap)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpMode;
+        private System.Windows.Forms.CheckBox chkTime;
+        private System.Windows.Forms.CheckBox chkDis;
+        private System.Windows.Forms.GroupBox grpPath;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.ComboBox cmbEnd;
+        private System.Windows.Forms.ComboBox cmbStart;
+        private System.Windows.Forms.GroupBox grpResult;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtDis;
+        private System.Windows.Forms.PictureBox picMap;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ChengduMetro3._0
+{
+    public partial class Mainfrm : Form
+    {
+        [DllImport("ChengduMetroDll.dll", EntryPoint = "Init", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Init(int Mode, int Points);
+        [DllImport("ChengduMetroDll.dll", EntryPoint = "Dijkstra", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Dijkstra(int Sta, int Points);
+        [DllImport("ChengduMetroDll.dll", EntryPoint = "Generate", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Generate(int Sta, int End);
+        [DllImport("ChengduMetroDll.dll", EntryPoint = "Distance", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Distance(int n, int End);
+        [DllImport("ChengduMetroDll.dll", EntryPoint = "Times", CallingConvention = CallingConvention.Cdecl)]
+        public static extern float Times(int n, int End);
+
+        static int
+            Mode = 1,
+            Sta = -1,
+            End = -1,
+            points = 0;
+        string name1 = "起始点",
+            name2 = "目的地";
+
+
+        float Time = 0;
+        int Dis = 0;
+        int j = 0;
+        List<Station> Stations = new List<Station>();   //站点链表
+        List<Station> Route = new List<Station>();      //途经站点链表
+
+        public Mainfrm()
+        {
+            InitializeComponent();
+        }
+
+        //获取出发点节点
+        private void cmbStart_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < points; i++)
+            {
+                if (cmbStart.Text == Stations[i].Station_Name)
+                {
+                    Sta = Stations[i].Station_ID;
+                    break;
+                }
+            }
+            txtResult.Clear();
+            txtDis.Text="预计里程";
+            txtTime.Text="预计耗时";
+            picMap.Refresh();
+        }
+        //获取目的地节点
+        private void cmbEnd_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < points; i++)
+            {
+                if (cmbEnd.Text == Stations[i].Station_Name)
+                {
+                    End = Stations[i].Station_ID;
+                    break;
+                }
+            }
+            txtResult.Clear();
+            txtDis.Text = "预计里程";
+            txtTime.Text = "预计耗时";
+            picMap.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //链接数据库
+            OleDbConnection MapInfo = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:\工作&学习\Homeworks\工程&实验\工程实践\工程实践_ChengduMetroQuery\ChengduMetroQuery 3.0\ChengduMetro\ChengduMetro3.0\bin\Debug\MetroDataBase.mdb");
+
+            MapInfo.Open();
+            if (MapInfo.State == ConnectionState.Open)
+            {
+                MessageBox.Show("已建立数据链接", "提示", MessageBoxButtons.OK);
+            }
+
+            //设定查询条目
+            string strSql = "select MapInfo.ID,MapInfo.StationName,MapInfo.x,MapInfo.y from MapInfo";
+
+            OleDbCommand sc1 = new OleDbCommand();
+            sc1.CommandText = strSql;
+            sc1.Connection = MapInfo;
+            OleDbDataReader sda = sc1.ExecuteReader();
+            while (sda.Read())
+            {
+                //StationName = sda.GetString(1);
+                //读取数据库中的id和相应站名
+                Station Info = new Station(sda.GetInt32(0), sda.GetString(1), sda.GetInt32(2), sda.GetInt32(3));
+                Stations.Add(Info);
+                //把站名导入到拉选框
+                cmbStart.Items.Add(sda.GetString(1));
+                cmbEnd.Items.Add(sda.GetString(1));
+                points++;
+            }
+            MessageBox.Show("已完成数据读取", "提示", MessageBoxButtons.OK);
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            if (!MyCheck(Sta, name1) || !MyCheck(End, name2))
+                return;
+
+            j = 0;
+            picMap.Refresh();
+            txtResult.Clear();
+            txtDis.Text = "预计里程";
+            txtTime.Text = "预计耗时";
+
+            //初始化权重并生成路径
+            Init(Mode, points);
+            Dijkstra(Sta, points);
+            Generate(Sta, End);
+            Dis = Distance(points, End);
+            Time = Times(points, End);
+            //MessageBox.Show("路径生成成功", "提示", MessageBoxButtons.OK);
+
+            //从Dijkstra算法生成的path文件中获取路径信息
+            StreamReader res = new StreamReader("Path.txt", Encoding.Default);
+            string file = "";
+            string[] result = new string[points];
+            while ((file = res.ReadLine()) != null)
+            {
+                Console.WriteLine(file.ToString());
+                string[] path = file.Split(',');
+                foreach (string i in path)
+                {
+                    result[j++] = i;
+                }
+            }
+            //比对站名ID并把所经过的站名存入路径链表
+            for (int i = 0; i < j; i++)
+            {
+                for (int j = 0; j < points; j++)
+                {
+                    if (int.Parse(result[i]) == Stations[j].Station_ID)
+                    {
+                        Route.Add(Stations[j]);
+                        break;
+                    }
+                }
+                //Console.WriteLine(result[i].ToString());
+            }
+
+            //在文本框中输出路径
+            for (int i = 0; i < j; i++)
+            {
+                if (i != j - 1)
+                    txtResult.Text += Route[i].Station_Name + "->";
+                else
+                    txtResult.Text += Route[i].Station_Name;
+            }
+
+            txtDis.Text = Dis.ToString() + " Km";
+            txtTime.Text = Time.ToString("f2") + " mins";
+            int current = 0, next = 1;
+            //进行路径绘制
+            while(current < j-1)
+            {
+                DrawPath(Route[current].x, Route[current].y, Route[next].x, Route[next].y);
+                current++;
+                next = current + 1;
+            }
+            res.Close();
+            Route.Clear();
+        }
+
+        private void chkDis_CheckedChanged(object sender, EventArgs e)
+        {
+            if ((sender as CheckBox).Checked == true)       //只允许单选
+            {
+                foreach (CheckBox chk in (sender as CheckBox).Parent.Controls)
+                {
+                    if (chk != sender)
+                    {
+                        chk.Checked = false;
+                    }
+                }
+            }
+            Mode = 1;
+            txtResult.Clear();
+            txtDis.Text = "预计里程";
+            txtTime.Text = "预计耗时";
+            picMap.Refresh();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult Quit = MessageBox.Show("退出？", "提示", MessageBoxButtons.OKCancel);
+            if (Quit == DialogResult.OK)
+                e.Cancel = false;
+            else
+                e.Cancel = true;
+        }
+
+        private void chkTime_CheckedChanged(object sender, EventArgs e)
+        {
+            if ((sender as CheckBox).Checked == true)       //只允许单选
+            {
+                foreach (CheckBox chk in (sender as CheckBox).Parent.Controls)
+                {
+                    if (chk != sender)
+                    {
+                        chk.Checked = false;
+                    }
+                }
+            }
+            Mode = 2;
+            txtResult.Clear();
+            txtDis.Text = "预计里程";
+            txtTime.Text = "预计耗时";
+            picMap.Refresh();
+        }
+
+        //自定义路径画笔
+        private void DrawPath(int x1, int y1, int x2, int y2)
+        {
+            //创建图形对象
+            Graphics path = picMap.CreateGraphics();
+            Pen pen = new Pen(Color.Black, 6);
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            //绘制路径高亮
+            path.DrawLine(pen, x1, y1, x2, y2);
+            pen.Dispose();
+            path.Dispose();
+        }
+
+        //自定义检查
+        bool MyCheck(int val, string name)
+        {
+            if (val == -1)
+            {
+                MessageBox.Show("请选择" + name + "!", "错误", MessageBoxButtons.OK);
+                return false;
+            }
+            return true;
+        }
+    }
+
+    //自定义Station类
+    public class Station
+    {
+        public int Station_ID = 0;
+        public string Station_Name = null;
+        public int x = 0;
+        public int y = 0;
+
+        public Station(int ID, string Name, int x, int y)
+        {
+            this.Station_ID = ID;
+            this.Station_Name = Name;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
